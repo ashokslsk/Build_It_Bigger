be5e4da4a7df6e025feef30d4c803d10146a6575
ashok.kumar <ashok.kumar@dunstconsulting.in>
2016-06-20 12:26:02 +0530

    Added Java library for showing random jokes.
    A Java library that provides jokes and displays the joke randomly in the textview.
    Added buuterknife and design libraries for the user interfaces design.

--- app/src/main/java/com/ashokslsk/builditbigger/MainActivity.java
+++ app/src/main/java/com/ashokslsk/builditbigger/MainActivity.java
@@ -1,13 +1,35 @@
 package com.ashokslsk.builditbigger;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.ashokslsk.JokeProvider;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    //Views
+    @Bind(R.id.main_tv_print_joke)
+    TextView tvPrintJoke;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.main_b_show_joke)
+    void getNewJoke() {
+        Log.d(TAG, "getNewJoke() called with: " + "");
+        tvPrintJoke.setText(JokeProvider.getNewJoke());
     }
 }
